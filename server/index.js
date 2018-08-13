2bbcd0d2c242f520cad238972e335bcf4719f1c0
L245854 <l245854@pn1706924.lanl.gov>
2018-08-13 13:03:44 -0600

    Basic server setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const massive = require('massive')
+require('dotenv').config()
+
+const app = express()
+
+//Bring variables off of process.env
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+
+//DB Connection
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    console.log('db is connected!')
+  })
+
+app.use(bodyParser.json())
+
+//API Endpoints
+
+app.listen(SERVER_PORT, ()=>{
+    console.log("listening on port:", SERVER_PORT)
+})
