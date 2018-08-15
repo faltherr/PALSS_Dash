1741203f509d8d2da70983132483b668e53bda3f
L245854 <l245854@pn1706924.lanl.gov>
2018-08-15 13:50:23 -0600

    Map with cluster functionality is working and basic landing page is styled

--- src/components/dashboard/Map.js
+++ src/components/dashboard/Map.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Map, TileLayer, CircleMarker, Popup } from 'react-leaflet'
+import { Map, TileLayer, Popup, Marker } from 'react-leaflet'
 import { connect } from 'react-redux'
-// import MarkerClusterGroup from 'react-leaflet-markercluster';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+import L from 'leaflet'
 
 
 class SimpleMap extends Component {
@@ -18,35 +19,73 @@ class SimpleMap extends Component {
         const map_position = [this.state.lat, this.state.lng];
         // console.log("Props accessible in map component", this.props, map_position)
 
-        const LeafletMarkers = this.props.event.map((element) => {
-            let { latitude: lat, longitude: lon, id, date, tabuilding } = element
-            return (
-                lat && lon && <CircleMarker key={id} center={[lat, lon]} fillColor="blue" fill="true" radius={8}>
-                    <Popup>
-                        <span> Date: {date} </span>
-                        <br />
-                        <span> Location: {tabuilding} </span>
-                    </Popup>
-                </CircleMarker>
-            )
+        // const LeafletMarkers = this.props.event.map((element) => {
+        //     let { latitude: lat, longitude: lon, id, date, tabuilding } = element
+        //     return (
+        //         lat && lon && <CircleMarker key={id} center={[lat, lon]} fillColor="blue" fill="true" radius={8}>
+        //             <Popup>
+        //                 <span> Date: {date} </span>
+        //                 <br />
+        //                 <span> Location: {tabuilding} </span>
+        //             </Popup>
+        //         </CircleMarker>
+        //     )
+        // })
 
-        })
-        console.log(47837374834783, LeafletMarkers)
-        return (
-            <div id="container">
-                <Map center={map_position} zoom={this.state.zoom} className="map-container">
-                    <TileLayer
-                        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                        url='https://api.mapbox.com/styles/v1/faltherr/cjku8ndjw044x2rpjzyvz6imf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZmFsdGhlcnIiLCJhIjoiY2prdTVxZW4zMDNmeDNrcWs3YmJrbTRwcCJ9.7Qq2GjLoD9jpqToGtpYVAA'
-                    />
-                    {/* <MarkerClusterGroup> */}
-                        {LeafletMarkers}
-                    {/* </MarkerClusterGroup> */}
-                </Map>
-            </div>
-        );
-    }
-}
+        //
+
+        const createClusterCustomIcon = function (cluster) {
+            return L.divIcon({
+                html: `<span>${cluster.getChildCount()}</span>`,
+                className: 'marker-cluster-custom',
+                iconSize: L.point(40, 40, true),
+            });
+        };
+
+        let markers = this.props.event.map(event => {
+            let { latitude: lat, longitude: lon, id, date, tabuilding } = event
+            return lat && lon && 
+                <Marker position={[lat,lon]} key={id}>
+                                 <Popup>
+                     <span> Id: {id} </span>
+                     <br/>
+                     <span> Date: {date} </span>
+                     <br/>
+                     <span> Location: {tabuilding} </span>
+                     <br/>
+                     <button>Edit</button> <button>Delete</button>
+                  </Popup>
+        </Marker>
+})
+
+//
+
+console.log(47837374834783, markers)
+
+return (
+    <div id="container">
+        <Map center={map_position} zoom={this.state.zoom} className="map-container" maxZoom={18} minZoom={10}>
+            <TileLayer
+                attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                url='https://api.mapbox.com/styles/v1/faltherr/cjku8ndjw044x2rpjzyvz6imf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZmFsdGhlcnIiLCJhIjoiY2prdTVxZW4zMDNmeDNrcWs3YmJrbTRwcCJ9.7Qq2GjLoD9jpqToGtpYVAA'
+            />
+
+
+
+            <MarkerClusterGroup
+                showCoverageOnHover={true}
+                // spiderfyDistanceMultiplier={2}
+                iconCreateFunction={createClusterCustomIcon}
+                // markers={markers}
+            >
+            {markers}
+            </MarkerClusterGroup>
+
+        </Map>
+    </div>
+);
+            }
+        }
 
 function mapStateToProps(state) {
     return {
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,11 +1,32 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+// import logo from '../../../public/logo_transparent.png'
+import { Link } from 'react-router-dom'
 
 export default class LandingPage extends Component {
-    render(){
+    render() {
         return (
-            <div>
-                Landing Page
+            <div className="landing-page-main-container">
+                <div className="landing-page-logo-container">
+                    <img src={process.env.PUBLIC_URL + '/logo_transparent.png'} alt="logo" className="logo" />
                 </div>
+                <div className="landing-page-text-container">
+                    <div>
+                        <h1 className="landing-page-app-title"> PALSS Dash </h1>
+                    </div>
+                    <hr />
+                    <div>
+                        <h4 className="landing-page-acronym"> Predictive Analytics for Laboratory Safety and Security </h4>
+                    </div>
+                </div>
+                <div className="landing-page-button-container">
+                    <Link to ='/Dashboard'>
+                    <button> Dashboard </button>
+                    </Link>
+                    <Link to ='/'>
+                    <button> Admin Login </button>
+                    </Link>
+                </div>
+            </div>
         )
     }
 }
